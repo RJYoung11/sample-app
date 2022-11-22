1b133fade0aa9898258115eea2f5857b8f10dd23
ryoung11 <ryoung@netlinkvoice.com>
2022-11-22 21:24:08 +0800

    Added route

--- db-connect.js
+++ db-connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://'HERE WILL BE YOUR EMAIL':' HERE WILL BE YOUR PASSWORD '@cluster0.h2o1d.mongodb.net/dbParkingSystem?retryWrites=true&w=majority";
-
+const uri = "mongodb+srv://raymondjay:iamlegendary11@cluster0.h2o1d.mongodb.net/dbParkingSystem?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://dbParkingSystem:@cluster0.g9l7uwl.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connect(uri, (err, db) => {
     console.log('Connected to database successfully!');
--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const connect = require('./db-connect');
 const app = express();
 const api = require('./routes/api')
+const cors = require('cors');
+const auth = require('./controllers/authController');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
